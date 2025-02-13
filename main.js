6ab0cbeb80803929f88a6b0ad277eca57cbba40f
Lemonnik6484 <LemonGamer6484@gmail.com>
2025-02-13 04:20:24 +0100

    indicators

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        minWidth: 400,
+        minHeight: 300,
         icon: path.join(__dirname, 'duck.png'),
         frame: false,
         webPreferences: {
